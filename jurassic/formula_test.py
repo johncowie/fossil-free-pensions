17287cf285bab18b7d7f5749de3d71bfb7adb736
John <john.a.cowie@gmail.com>
2017-09-27 20:55:18 +0100

    Added formula generation for verification column

--- jurassic/formula_test.py
+++ jurassic/formula_test.py
@@ -1,5 +1,12 @@
 import formula
 
+def test_verification_formula():
+    expected = '=IF(OR(NOT(A3="0"),NOT(B3="0")),C3,A3)'
+    assert expected == formula.verification_formula(3, 'A', 'B', 'C')
+
+    expected = '=IF(OR(NOT(D12="0"),NOT(E12="0")),B12,D12)'
+    assert expected == formula.verification_formula(12, 'D', 'E', 'B')
+
 def test_pattern_match_formula():
     expected = '=IFS(REGEXMATCH(A1, "abc"), "A", REGEXMATCH(A1, "def"), "B")'
     assert formula.pattern_match_formula("A1", [["A", "abc"], ["B", "def"]]) == expected
