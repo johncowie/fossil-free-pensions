99ffc7ae5498f37455fa8b6d63ee16312fc6652a
John <john.a.cowie@gmail.com>
2018-06-04 17:36:38 +0100

    Can write out stage5 files, and then generate spreadsheets

--- jurassic/main.py
+++ jurassic/main.py
@@ -1,16 +1,18 @@
 from spreadsheet import Spreadsheet
 from progress import FileList, FileCheckList
-from stage5 import create_all_spreadsheets_sf, read_stage3, write_stage5, process_stage3
+import stage5
 import json
 import argparse
 import time
 import shutil
 import os
 
-emails = ['john.a.cowie@gmail.com',
-          'anna@platformlondon.org',
-          'anna.galkina.tation@gmail.com',
-          "sarahshoraka77@gmail.com"]
+# Pull these into separate files
+# emails = ['john.a.cowie@gmail.com',
+#            'anna@platformlondon.org',
+#           'anna.galkina.tation@gmail.com',
+#           "sarahshoraka77@gmail.com"
+# ]
 #
 # emails = ['john.a.cowie@gmail.com']
 
@@ -26,6 +28,7 @@ def generate_url(folder_id, file_id):
     # https://docs.google.com/spreadsheets/d/1syJoVb9KOrATW0ttw4cpEf0y9ee1Zx6fm1Q-F9pt4Rw/
     return "https://docs.google.com/spreadsheets/d/{1}/".format(folder_id, file_id)
 
+# TODO add this to CLI methods
 def get_urls(api, folder_name):
     folder_id = api.find_folder(folder_name)['id']
     files = api.find_files_in_folder(folder_name)
@@ -34,58 +37,109 @@ def get_urls(api, folder_name):
         lines.append(f['name'] + " - " + generate_url(folder_id, f['id']))
     return lines
 
-def read(args):
-    # get list of urls from api
-    api = Spreadsheet('creds/stage5_3.json')
-
-    # TODO pass in arguments themselves 
-    reset = args.reset
-    direc = args.path
-    stage3_dir = direc + '/stage3'
+class CLI:
     
-    if not os.path.exists(stage3_dir) or args.reset:
-        shutil.rmtree(stage3_dir, True)
-        os.mkdir(stage3_dir)
-        os.mkdir(stage3_dir + '/data')
+    def __init__(self, api, path):
+        self._api = api
+        self._path = path
+        self._stage3_dir = path + '/stage3'
+        self._stage3_data = path + '/stage3/data'
+        self._stage3_progress = path + '/stage3/progress.json'
+        self._stage5_data = path + '/stage5/data'
     
-    fcl = FileCheckList(stage3_dir + '/progress.json')
-    if not fcl.has_todos():
-        files = api.find_files_in_folder('Stage 3 JC created sheets')
-        fcl.set_todos(files)
+    def read(self, reset):
+    
+        if not os.path.exists(self._stage3_dir) or reset:
+            shutil.rmtree(self._stage3_dir, True)
+            os.mkdir(self._stage3_dir)
+            os.mkdir(self._stage3_data)
+    
+        fcl = FileCheckList(self._stage3_progress)
+        if not fcl.has_todos():
+            files = self._api.find_files_in_folder('Stage 3 JC created sheets')
+            fcl.set_todos(files)
 
-    while (fcl.get_next_todo() != None):
-        todo = fcl.get_next_todo()
-        name = todo['name']
-        ss_id = todo['id']
-        ss = api.open_by_id(ss_id)
-        data = {}
-        data['data'] = ss.worksheet('Full Data').get_all_records()
-        data['pooled'] = ss.worksheet('Pooled').get_all_records()
-        print('Writing ' + name + '.json')
-        with open(stage3_dir + '/data/' + name + '.json', 'w') as f:
-            f.write(json.dumps(data))
-        fcl.mark_as_done(todo)
-        time.sleep(3)
+        while (fcl.get_next_todo() != None):
+            todo = fcl.get_next_todo()
+            name = todo['name']
+            ss_id = todo['id']
+            ss = self._api.open_by_id(ss_id)
+            data = {}
+            data['data'] = ss.worksheet('Full Data').get_all_records()
+            data['pooled'] = ss.worksheet('Pooled').get_all_records()
+            print('Writing ' + name + '.json')
+            with open(self._stage3_data + '/' + name + '.json', 'w') as f:
+                f.write(json.dumps(data))
+            fcl.mark_as_done(todo)
+            time.sleep(3)
         
 
-def transform(args):
-    print('Transform')
+    def transform(self):
 
-def write(args):
-    print('Writing')
+        # Read formula spreadsheets
+        oil = self._api.get_records('Categories', 'Oil')
+        coal = self._api.get_records('Categories', 'Coal')
+
+        # Wipe existing stage 5 directory
+        shutil.rmtree(self._stage5_data, True)
+    
+        # Create stage5 directory
+        os.mkdir(self._stage5_data)
+    
+        # get list of files in stage 3 directory
+        files = os.listdir(self._stage3_data)
+    
+        # For each file in directory
+        for f in files:
+            # Transform data
+            print('Transforming ' + f)
+            fund_name = f.replace('.json', '')
+            input_data = None
+            with open(self._stage3_data + '/' + f) as fl:
+                input_data = json.loads(fl.read())
+      
+            output_data = stage5.gen_spreadsheet(fund_name, input_data, oil, coal)
+            with open(self._stage5_data + '/' + f, 'w+') as fl:
+                fl.write(json.dumps(output_data))
+
+    def write(self):
+
+        folder_name = 'JCFRACKTEST'
+
+        folder_id = stage5.prepare_folder(self._api, folder_name, False) 
+        # for each file in files
+        files = os.listdir(self._stage5_data)
+
+        can_edit = True
+
+        emails = ['john.a.cowie@gmail.com']
+        
+        for f in files:
+            fund_name = f.replace('.json', '')
+            data = None
+            with open(self._stage5_data + '/' + f) as fl:
+                data = json.loads(fl.read())
+            stage5.create_spreadsheet(self._api, folder_id, fund_name, data, emails, can_edit)
+            # call create spreadsheet with data
+            
 
 def cli():
     parser = argparse.ArgumentParser()
     parser.add_argument("command", help="read/transform/write")
     parser.add_argument("--reset", action='store_true')
     parser.add_argument("--path", default='testingDataDirec')
+    parser.add_argument("--creds", default='creds/stage5_3.json')
     args = parser.parse_args()
+    
+    api = Spreadsheet(args.creds)
+    cli = CLI(api, args.path)
+
     if args.command == 'read':
-        read(args)
+        cli.read(args.reset)
     elif args.command == 'transform':
-        transform(args)
+        cli.transform()
     elif args.command == 'write':
-        write(args)
+        cli.write()
     else:
         print ('Unrecognised command')
 
@@ -116,6 +170,6 @@ cli()
 # with open('urls.txt', 'w') as f:
   #   f.write("\n".join(sorted(get_urls(api, 'Stage 5'))))
 
-
-# fid = api.find_folder('Stage 5')['id']
+# api = Spreadsheet('creds/stage5_3.json')
+# fid = api.find_folder('JCFRACKTEST')['id']
 # api.transfer_ownership(fid, "john.a.cowie@gmail.com")
